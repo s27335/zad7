181101a6cbc997b1f52cd25c53895e3399c09984
s27335 <s27335@pjwstk.edu.pl>
2024-05-07 17:46:11 +0200

    warehouse controller fix

--- WebApp3/WebApp3/Controllers/WarehouseController.cs
+++ WebApp3/WebApp3/Controllers/WarehouseController.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using WebApp3.Services;
 
 namespace WebApp3.Controllers;
 
 [Route("api/warehouse")]
 [ApiController]
-public class WarehouseController
+public class WarehouseController : ControllerBase
 {
     private IWarehouseService _warehouseService;
 
